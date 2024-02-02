8fac7f286b7077d2c828649c14d8c7c0d5aed2de
Olena Franchuk <olenafranchuk@knu.ua>
2024-02-02 11:29:19 +0200

    Prime number

--- utils.py
+++ utils.py
@@ -27,3 +27,22 @@ def NSD(a, b):
         a, b = b, (a % b)
 
     return a
+
+
+
+def is_prime(number):
+    if number < 2:
+        return False
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+# Зчитуємо число від користувача
+user_input = int(input("Введіть число: "))
+
+# Перевіряємо, чи є число простим і виводимо результат
+if is_prime(user_input):
+    print(f"{user_input} - просте число")
+else:
+    print(f"{user_input} - не є простим числом")
